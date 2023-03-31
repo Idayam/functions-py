e5401a2799d792fc76eb08c515429eab8101d4a2
Idayam <mzeeidaya@gmail.com>
2023-03-31 21:50:15 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,21 @@
+def add(a,b):
+    answer=a+b
+    return answer
+
+def subtract(x,y):
+    answer=x-y
+    return answer
+
+def divide(v,w):
+    answer=v/w
+    return answer
+
+def multiply(s,t):
+    answer=s*t
+    return answer
+
+def remainder(c,d):
+    answer=c%d
+    return answer
+
+    
